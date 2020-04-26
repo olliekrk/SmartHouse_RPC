0a55810e13373dda5d340246badf76a0c12c0f87
olliekrk <krolik.olgierd@gmail.com>
2020-04-26 12:04:29 +0200

    Python cmd client init

--- py-client/ice_rpc_client.py
+++ py-client/ice_rpc_client.py
@@ -1,26 +1,17 @@
 import sys
 
 import Ice
-import Home
+
+from ice_rpc_cmd import DeviceCmd
 
 SERVER_PORT_CMD = 'tcp -h localhost -p 10000'
 
 
-def identity_string(name, category=""):
-    return '{}/{}'.format(category, name)
-
-
 def run():
     with Ice.initialize(sys.argv) as communicator:
-        identity = identity_string('Hello', 'home')
-        print("Accessing object: " + identity)
-        proxy = communicator.stringToProxy(identity + ':' + SERVER_PORT_CMD)
-        obj = Home.HomeStatusPrx.checkedCast(proxy)
-        print(dir(obj))
-        if not obj:
-            raise RuntimeError('Failed to access object reference')
+        DeviceCmd(communicator, SERVER_PORT_CMD).cmdloop()
 
 
 if __name__ == '__main__':
-    print('Starting the client')
+    print('Starting the client...')
     run()
---
+++ py-client/ice_rpc_cmd.py
@@ -0,0 +1,77 @@
+from enum import Enum
+import sys
+import argparse
+import cmd2
+import Home
+
+INTRO = 'Smart Home RPC remote controller\nv0.1 2020\n'
+PROMPT = 'Home> '
+
+
+class DeviceCategory(Enum):
+    home = 1
+    wall_camera = 2
+    drone_camera = 3
+    grass_mower = 4
+    fridge = 5
+
+
+def device_name_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--name', required=True, action='store_true', help='Name of the device to access')
+    return parser
+
+
+class RpcController:
+    def __init__(self, communicator, server_cmd):
+        self.communicator = communicator
+        self.server_cmd = server_cmd
+        self.proxies = {}
+
+    def access_object(self, name, category):
+        identity = '{}/{}'.format(category.name, name)
+        if identity not in self.proxies:
+            self.proxies[identity] = self.__get_object_access(identity, category)
+        return self.proxies[identity]
+
+    def __get_object_access(self, identity, category):
+        print("Attempt to access object: " + identity)
+        proxy = self.communicator.stringToProxy(identity + ':' + self.server_cmd)
+        obj = None
+        if category == DeviceCategory.home:
+            obj = Home.HomeStatusPrx.checkedCast(proxy)
+        elif category == DeviceCategory.wall_camera:
+            obj = Home.Garden.WallCameraControllerPrx.checkedCast(proxy)
+        elif category == DeviceCategory.drone_camera:
+            obj = Home.Garden.DroneCameraControllerPrx.checkedCast(proxy)
+        elif category == DeviceCategory.grass_mower:
+            obj = Home.Garden.GrassMowerControllerPrx.checkedCast(proxy)
+        elif category == DeviceCategory.fridge:
+            obj = Home.Kitchen.FridgeControllerPrx.checkedCast(proxy)
+        if not obj:
+            raise RuntimeError('Failed to access object')
+        return obj
+
+
+class DeviceCmd(cmd2.Cmd):
+    prompt = PROMPT
+
+    def __init__(self, communicator, server_cmd):
+        self.communicator = communicator
+        self.server_cmd = server_cmd
+        self.controller = RpcController(communicator, server_cmd)
+        super().__init__()
+
+    def cmdloop(self, intro=INTRO):
+        return cmd2.Cmd.cmdloop(self, intro)
+
+    @cmd2.with_argparser(device_name_parser())
+    def do_list_devices(self, args):
+        """List all currently available and instantiated devices."""
+        proxy = self.controller.access_object(args.name, DeviceCategory.home)
+        print(proxy.getActiveDevices)
+
+    @staticmethod
+    def do_exit(args):
+        """Exit the program"""
+        sys.exit(0)
